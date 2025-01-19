aefb2ebfeea0b38eb51aa6e3d9c7d9d5e016a545
Dustin Firebaugh <dafirebaugh@gmail.com>
2025-01-19 14:34:06 -0500

    fixing CNAME problem

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,5 +9,7 @@ fi
 
 mkdir -p .dist/web/
 
+echo dustinfirebaugh.com >> CNAME
+
 mdbook build ./content -d ../.dist/web/
 mdbook build ./books/20-hours-to-golang -d ../../.dist/web/books/20-hours-to-golang
